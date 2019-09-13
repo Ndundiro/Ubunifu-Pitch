e45a6deec61dc06aeae00364e4ae6d9b575afb35
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-13 12:36:26 +0300

    Adds a file forms.py and defines the registration and login forms functionality structure,with input fields and validators.

--- ubunifu.py
+++ ubunifu.py
@@ -1,14 +1,16 @@
 from flask import Flask,render_template
-
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = '450922fba6e46eeb36b12d8a7635aa92836d3dbe'
 
 @app.route("/")
 def index():
     title = "Ubunifu;the creatives haven"
     return render_template("index.html",title=title)
 
-@app.route("/About")
-def about():
+@app.route("/register")
+def register():
+    form = RegistrationForm()
     return render_template("about.html")
 
 
