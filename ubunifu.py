e5dffdcabce81193d3bb3549ab1e7bd731b197ca
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-13 11:38:58 +0300

    Adds  about.html  and base.html files.The base defines the nav and the footer where all other templates inherit.

--- ubunifu.py
+++ ubunifu.py
@@ -3,12 +3,13 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return "Hello ip"
+def index():
+    title = "Ubunifu;the creatives haven"
+    return render_template("index.html",title=title)
 
 @app.route("/About")
 def about():
-    return "About this"
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
